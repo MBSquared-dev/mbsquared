2f9c13ba56bfb37ea1c9749101bf4e6ef5fac140
cadin7 <erdei_mark@yahoo.com>
2025-04-10 11:44:03 +0200

    Base updated for GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/mbsquared/',
   server: {
     host: "::",
     port: 3000,
